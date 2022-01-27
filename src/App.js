33d6836a2f9e65e5f53a66116f8fd17682d1bdcc
Franco de Guzman <tangkekofranco06@gmail.com>
2022-01-28 00:36:45 +0800

    DONE

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import data from './data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="grid-container">
+      <header className="row">
+          <div>
+              <a href="index.html" className="brand">KaBuYours</a>
+          </div>
+          <div>
+              <a href="/cart"> Cart</a>
+              <a href="/signin">Sign In</a>
+          </div>
       </header>
+
+      <main>
+        <div className="row center">
+          {
+            data.products.map(product => (<div key={product._id} className="card">
+            <a href={`/product/${product._id}`}>
+            <img className="medium" src={product.image} alt={product.name}/>
+            </a>
+            <div className="card-body">
+                <a href={`/product/${product._id}`}><h2>{product.name}</h2></a>
+                <div className="rating">
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                </div>
+                <div className="price">
+                 {product.price}
+                </div>
+            </div>
+      </div>))
+          }
+              
+          </div>
+        </main>
+        <footer className="row center"> All rights reserved
+        </footer>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,105 @@
+
+const data={
+    products:[
+        {
+            _id:"1",
+            name: "Tadashi",
+            category: "Horse",
+            image: "https://pegaxy.io/wp-content/uploads/2021/09/zan.png",
+            price: '$200',
+            brand: "Pegaxy",
+            rating: 4.5,
+            numReview: 10,
+            description: "High quality product"
+        }, 
+        {
+            _id:"2",
+            name: "Oyama",
+            category: "Horse",
+            image: "/images/steelhorse.png",
+            price: '$230',
+            brand: "Pegaxy",
+            rating: 4.5,
+            numReview: 10,
+            description: "High quality product"
+        },
+        {
+            _id:"3",
+            name: "Fukunaga",
+            category: "Horse",
+            image: "/images/pinkhorse.png",
+            price: '$430',
+            brand: "Pegaxy",
+            rating: 4.5,
+            numReview: 10,
+            description: "High quality product"
+        },
+        {
+            _id:"4",
+            name: "Haru and Eri",
+            category: "Horse",
+            image: "/images/siblingshorse.png",
+            price: '$250',
+            brand: "Pegaxy",
+            rating: 4.5,
+            numReview: 10,
+            description: "High quality product"
+        },
+        {
+            _id:"5",
+            name: "Rengoku",
+            category: "Horse",
+            image: "/images/speedsterhorse.png",
+            price: '$500',
+            brand: "Pegaxy",
+            rating: 4.5,
+            numReview: 10,
+            description: "High quality product"
+        },
+        {
+            _id:"6",
+            name: "Kabayong Manager",
+            category: "Horse",
+            image: "/images/intelligenthorse.png",
+            price: '$130',
+            brand: "Pegaxy",
+            rating: 4.5,
+            numReview: 10,
+            description: "High quality product"
+        },
+        {
+            _id:"7",
+            name: "Donkey",
+            category: "Horse",
+            image: "/images/donkey.png",
+            price: '$100',
+            brand: "Pegaxy",
+            rating: 4.5,
+            numReview: 10,
+            description: "High quality product"
+        },
+        {
+            _id:"8",
+            name: "Maximilienne",
+            category: "Horse",
+            image: "/images/blackhorse.png",
+            price: '$200',
+            brand: "Pegaxy",
+            rating: 4.5,
+            numReview: 10,
+            description: "High quality product"
+        },
+        {
+            _id:"9",
+            name: "Knight",
+            category: "Horse",
+            image: "/images/whitehorse.png",
+            price: '$150',
+            brand: "Pegaxy",
+            rating: 4.5,
+            numReview: 10,
+            description: "High quality product"
+        }
+    ]
+}
+export default data;
